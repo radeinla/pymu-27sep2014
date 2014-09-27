dcb55da7ac591fefe4bcc2340649da4eafa0d0a5
Reginald Eli A. Deinla <aldrineg@localhost.localdomain>
2014-09-27 15:06:00 +0800

    redirect daw

--- app/handlers.py
+++ app/handlers.py
@@ -6,15 +6,16 @@ class MainHandler(RequestHandler):
 
 class LoginHandler(RequestHandler):
     def get(self):
-        self.render("login.html", message="Please login.", error="")
+        message = self.get_argument("message", "Please login.")
+        self.render("login.html", message=message)
 
     def post(self):
         user = self.get_argument("username", "")
         password = self.get_argument("password", "")
         if user == 'admin' and password == 'password':
-            self.render("login.html", message="succ", error="")
+            self.redirect("/")
         else:
-            self.render("login.html", message="", error="fail")
+            self.redirect("/login?message=fail")
 
 class GreetHandler(RequestHandler):
     def get(self, name=None):
